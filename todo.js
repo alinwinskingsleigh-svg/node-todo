c1146c877ee7c06a906376f92f0ba8c45d0f1318
myori <alinwinskingsleigh@snu.ac.kr>
2026-03-08 16:37:11 +0900

    feat: addTodo 함수 기능 구현 (어려운 점: newId를 어떤 방식으로 정해야할지, 그것을 어떻게 코드로 옮길지 생각해내는 것이 어려웠다)

--- todo.js
+++ todo.js
@@ -15,3 +15,17 @@ function saveTodos(todos) {
     fs.writeFileSync('todos.json', JSON.stringify(todos, null, 2));
 }
 
+//addTodo 함수
+function addTodo(Title){
+    const todos = loadTodos();
+    const newId = todos.length > 0 ? todos[todos.length - 1].id + 1 : 1;
+    const newTodo = {
+        id: newId,
+        title: Title,
+        done: false
+    };
+    todos.push(newTodo);
+    saveTodos(todos);
+
+    console.log(`Todo가 추가되었습니다!😆: [${Title}]`);
+}
